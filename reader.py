62d5bceb3217558b016c712f2087d4b928860b5e
Vedant Kumar <vk@vedantk.com>
2013-07-04 18:37:54 -0700

    Added basic ArgumentParser logic

--- reader.py
+++ reader.py
@@ -4,10 +4,23 @@ import ast
 import sys
 import argparse
 
+desc = '''
+reader.py -- A reader for 61A
+'''
+
+def grade(module, func_name):
+    pass
+
 def node_count(module):
     pass
     
 if __name__ == '__main__':
-    # argparse
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('source_file')
+    parser.add_argument('function_name')
+    args = parser.parse_args()
 
-    module = ast.parse(...)
+    with open(args.source_file, 'r') as f:
+        module = ast.parse(f.read())
+
+    grade(module, args.function_name)
